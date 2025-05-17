b604eaf2e6b4bfe834739b6a25c53dffd18af792
seungki <sysh9498@gmail.com>
2025-05-17 14:19:52 +0900

    feat: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import Book from "./components/Book.tsx";
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Survey from "./pages/Survey.tsx";
 
 function App() {
-    return <Book />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Book />} />
+        <Route path="/survey" element={<Survey />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
