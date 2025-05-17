84b4cfa0b172f5d2076dde1412511efe67bebc67
seungki <sysh9498@gmail.com>
2025-05-17 18:33:22 +0900

    refactor: simplify Book and Page components by removing unused props and comments

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -6,17 +6,17 @@ import { StepInterface } from "./ProgressBar";
 
 const TOTAL_PAGES = 9;
 
-function Book({ step, setStep }: StepInterface) {
+function Book({ step }: StepInterface) {
   const [open, setOpen] = useState(false);
 
   const handleClickCover = () => {
     setOpen(prev => !prev);
   };
 
-  const handleClickPage = () => {
-    if (step >= TOTAL_PAGES - 1) return;
-    setStep(prev => prev + 1);
-  };
+  // const handleClickPage = () => {
+  //   if (step >= TOTAL_PAGES - 1) return;
+  //   setStep(prev => prev + 1);
+  // };
 
   useEffect(() => {
     setOpen(false);
@@ -36,12 +36,8 @@ function Book({ step, setStep }: StepInterface) {
         <div className="cover" onClick={handleClickCover} style={{ zIndex: TOTAL_PAGES + 1 }}></div>
 
         {Array.from({ length: TOTAL_PAGES }).map((_, i) => (
-          <Page isFlipped={i < step} onClick={handleClickPage} zIndex={TOTAL_PAGES + (i < step ? i + 1 : -i)}>
-            <h2>Page {i + 1}</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil magni laudantium beatae quia. Recusandae, fuga quas
-              consectetur perferendis aperiam esse velit veniam ducimus? Quisquam consequatur perferendis quidem quia, recusandae ab!
-            </p>
+          <Page isFlipped={i < step}  zIndex={TOTAL_PAGES + (i < step ? i + 1 : -i)}>
+            <img src={`/images/survey/survey_${i+1}.png`} />
           </Page>
         ))}
 
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,18 +5,17 @@ import './Page.css';
 interface PageProps {
     isFlipped: boolean;
     children: ReactNode;
-    onClick: () => void;
     zIndex: number;
 }
 
-function Page({isFlipped, children, onClick, zIndex}: PageProps) {
+function Page({isFlipped, children, zIndex}: PageProps) {
     return (
-        <div className={clsx(`page`, {flipped: isFlipped})} style={{zIndex}} onClick={onClick}>
+        <div className={clsx(`page`, {flipped: isFlipped})} style={{zIndex}} >
             <div className="front-page">
                 {children}
             </div>
             <div className="back-page">
-                <p>back page</p>
+                {/* <p>back page</p> */}
             </div>
         </div>
     );
