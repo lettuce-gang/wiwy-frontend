d3ba0107b975ecee2592acebc984cdd418e841c0
won983212 <won983212@naver.com>
2025-05-17 17:03:28 +0900

    feat: 이름 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import Book from "./components/Book.tsx";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Survey from "./pages/Survey.tsx";
 import Result from "./pages/Result.tsx";
+import Intro from "./pages/Intro.tsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Book />} />
+        <Route path="/" element={<Intro />} />
         <Route path="/survey" element={<Survey />} />
         <Route path="/result" element={<Result />} />
       </Routes>
