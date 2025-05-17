2897c94ae552b33e34145e41b06a1f68f1bd3428
seungki <sysh9498@gmail.com>
2025-05-17 14:20:52 +0900

    feat: 서베이 페이지 추가

---
+++ src/pages/Survey.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProgressBar from "../components/ProgressBar";
+
+function Survey() {
+    return (
+        <div>
+            <ProgressBar />
+        </div>
+    );
+}
+
+export default Survey;
