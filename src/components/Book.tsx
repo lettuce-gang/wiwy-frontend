2e2b9f55f97073c0bcd79b52bafa59efb4be832f
seungki <sysh9498@gmail.com>
2025-05-17 17:07:55 +0900

    feat: Book 컴포넌트 수정

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,41 +1,54 @@
 import clsx from "clsx";
-import {useState} from 'react';
-import './Book.css';
-import Page from './Page';
+import { useEffect, useState } from "react";
+import "./Book.css";
+import Page from "./Page";
+import { StepInterface } from "./ProgressBar";
 
 const TOTAL_PAGES = 9;
 
-function Book() {
-    const [open, setOpen] = useState(false);
-    const [page, setPage] = useState(0);
+function Book({ step, setStep }: StepInterface) {
+  const [open, setOpen] = useState(false);
 
-    const handleClickCover = () => {
-        setOpen(prev => !prev);
+  const handleClickCover = () => {
+    setOpen(prev => !prev);
+  };
+
+  const handleClickPage = () => {
+    if (step >= TOTAL_PAGES - 1) return;
+    setStep(prev => prev + 1);
+  };
+
+  useEffect(() => {
+    setOpen(false);
+
+    const timer = window.setTimeout(() => {
+      setOpen(true);
+    }, 1500);
+
+    return () => {
+      clearTimeout(timer);
     };
+  }, []);
 
-    const handleClickPage = () => {
-        if (page >= TOTAL_PAGES - 1) return;
-        setPage(prev => prev + 1);
-    }
+  return (
+    <>
+      <div className={clsx("book", { open })}>
+        <div className="cover" onClick={handleClickCover} style={{ zIndex: TOTAL_PAGES + 1 }}></div>
 
-    return (
-        <>
-            <div className={clsx("book", {open})}>
-                <div className="cover" onClick={handleClickCover} style={{zIndex: TOTAL_PAGES + 1}}></div>
+        {Array.from({ length: TOTAL_PAGES }).map((_, i) => (
+          <Page isFlipped={i < step} onClick={handleClickPage} zIndex={TOTAL_PAGES + (i < step ? i + 1 : -i)}>
+            <h2>Page {i + 1}</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil magni laudantium beatae quia. Recusandae, fuga quas
+              consectetur perferendis aperiam esse velit veniam ducimus? Quisquam consequatur perferendis quidem quia, recusandae ab!
+            </p>
+          </Page>
+        ))}
 
-                {Array.from({length: TOTAL_PAGES}).map((_, i) => (
-                    <Page isFlipped={i < page} onClick={handleClickPage} zIndex={TOTAL_PAGES + (i < page ? i + 1 : -i)}>
-                        <h2>Page {i + 1}</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil magni laudantium beatae quia.
-                            Recusandae, fuga quas consectetur perferendis aperiam esse velit veniam ducimus? Quisquam
-                            consequatur perferendis quidem quia, recusandae ab!</p>
-                    </Page>
-                ))}
-
-                <div className="back-cover"></div>
-            </div>
-        </>
-    )
+        <div className="back-cover"></div>
+      </div>
+    </>
+  );
 }
 
-export default Book
+export default Book;
