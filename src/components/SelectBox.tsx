02d30e74cafef2e2b11fe17ab149e7fe036d4ff7
seungki <sysh9498@gmail.com>
2025-05-17 18:33:40 +0900

    feat: enhance SelectBox component with button functionality and styling improvements

--- src/components/SelectBox.tsx
+++ src/components/SelectBox.tsx
@@ -3,31 +3,30 @@ import styled from "styled-components";
 import { questionData } from "../data/questionData";
 
 interface SelectBoxInterface {
-    step: number,
-    setStep: React.Dispatch<React.SetStateAction<number>>,
+  step: number;
+  setStep: React.Dispatch<React.SetStateAction<number>>;
 }
 
 function SelectBox({ step, setStep }: SelectBoxInterface) {
   const [selectedIdx, setSelectedIdx] = useState<number | null>(null);
-  const options = questionData[step] || {};
+  const [btnDisabled, setBtnDisabled] = useState<boolean>(false);
+  const options = questionData[step + 1] || {};
 
   const onClickHandler = (idx: number) => {
     setSelectedIdx(idx);
-    setTimeout(()=> {
-        setSelectedIdx(null);
-        setStep(prev => prev + 1);
-    },2000)
+    setBtnDisabled(true);
+    setTimeout(() => {
+      setSelectedIdx(null);
+      setStep(prev => prev + 1);
+      setBtnDisabled(false);
+    }, 2000);
   };
   return (
     <Wrapper>
       {Object.entries(options).map(([key, text]) => {
         const idx = Number(key) - 1;
         return (
-          <Selector
-            key={key}
-            selected={selectedIdx === idx}
-            onClick={() => onClickHandler(idx)}
-          >
+          <Selector key={key} selected={selectedIdx === idx} onClick={() => onClickHandler(idx)} disabled={btnDisabled}>
             {text}
           </Selector>
         );
@@ -48,14 +47,41 @@ const Wrapper = styled.div`
   margin: 33px auto;
 `;
 
-const Selector = styled.div<{ selected: boolean }>`
+const Selector = styled.button<{ selected: boolean }>`
   width: 100%;
   height: 52px;
   border-radius: 10px;
   border: ${({ selected }) =>
     selected
-      ? "1.5px solid rgba(255, 255, 255, 0.50)" // 선택된 경우
+      ? "2.5px solid rgba(255, 255, 255, 0.50)" // 선택된 경우
       : "1px solid rgba(255, 255, 255, 0.08)"}; // 기본
   background: rgba(255, 255, 255, 0.2);
   background-blend-mode: overlay;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: #fff;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -0.15px;
+
+  &:hover {
+    border: 2.5px solid rgba(255, 255, 255, 0.5);
+  }
+
+  &:active {
+    border: 2.5px solid rgba(255, 255, 255, 0.5);
+  }
+
+  &:hover {
+    ${({ selected }) =>
+      selected
+        ? "2.5px solid rgba(255, 255, 255, 0.50)"
+        : "1px solid rgba(255, 255, 255, 0.08)"};
+  }
 `;
